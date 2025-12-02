ce5abb70c5332ee82f8c7f73addd8df457bc0956
phuongnguyen1234 <nguyenhoangducphuong1@gmail.com>
2025-12-02 10:38:40 +0700

    v2.0: fixed content width of journal card

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.7"
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
